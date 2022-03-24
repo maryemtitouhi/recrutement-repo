4fa48ada5ca551e8bd84d89722203c7eba810142
mouradbounasri@gmail.com <mouradbounasri@gmail.com>
2022-03-24 12:55:29 +0100

    first front commit

---
+++ recrutement-front/src/app/app.routing.ts
@@ -0,0 +1,79 @@
+import {Route} from '@angular/router';
+import {LayoutComponent} from 'app/layout/layout.component';
+
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+
+    // Redirect empty path to '/example'
+    {path: '', pathMatch: 'full', redirectTo: 'example'},
+
+    // Redirect signed in user to the '/example'
+    //
+    // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+
+    // Auth routes for guests
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {
+                path: 'sign-in',
+                loadChildren: () => import('app/modules/auth/sign-in/sign-in.module').then(m => m.AuthSignInModule)
+            },
+            {
+                path: 'sign-up',
+                loadChildren: () => import('app/modules/auth/sign-up/sign-up.module').then(m => m.AuthSignUpModule)
+            }
+        ]
+    },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {
+                path: 'sign-out',
+                loadChildren: () => import('app/modules/auth/sign-out/sign-out.module').then(m => m.AuthSignOutModule)
+            }
+        ]
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {
+                path: 'home',
+                loadChildren: () => import('app/modules/landing/home/home.module').then(m => m.LandingHomeModule)
+            },
+        ]
+    },
+
+    // Admin routes
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'example',
+                loadChildren: () => import('app/modules/admin/example/example.module').then(m => m.ExampleModule)
+            },
+        ]
+    }
+];
